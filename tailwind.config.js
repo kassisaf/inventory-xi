40e8f7e4c19d2d77f8a628e5d8761a5b63925cb0
Andrew Kassis <kassisaf@gmail.com>
2024-05-27 20:25:57 -0700

    Install NextUI table

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,7 +2,12 @@ const { nextui } = require('@nextui-org/react')
 
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: ['./src/**/*.{js,ts,jsx,tsx}', './out/**/*.{js,ts,jsx,tsx}', './node_modules/@nextui-org/theme/dist/**/*.{js,ts,jsx,tsx}'],
+  content: [
+    './src/**/*.{js,ts,jsx,tsx}',
+    './out/**/*.{js,ts,jsx,tsx}',
+    //'./node_modules/@nextui-org/theme/dist/**/*.{js,ts,jsx,tsx}'
+    './node_modules/@nextui-org/theme/dist/components/table.js',
+  ],
   theme: {
     extend: {},
   },
