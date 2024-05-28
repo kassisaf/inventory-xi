47ff79d671923e056a9c2631900ee37a25e96d61
Andrew Kassis <kassisaf@gmail.com>
2024-05-27 21:01:55 -0700

    Install tailwind/prettier plugin

--- src/renderer/src/App.tsx
+++ src/renderer/src/App.tsx
@@ -4,32 +4,12 @@ import electronLogo from './assets/electron.svg'
 import { NextUIProvider } from '@nextui-org/react'
 
 function App(): JSX.Element {
-  const ipcHandle = (): void => window.electron.ipcRenderer.send('ping')
-
   return (
     <NextUIProvider>
-      <img alt="logo" className="logo" src={electronLogo} />
-      <div className="creator">Powered by electron-vite</div>
-      <div className="text">
-        Build an Electron app with <span className="react">React</span>
-        &nbsp;and <span className="ts">TypeScript</span>
+      <div className="flex flex-col items-center">
+        <img alt="logo" className="logo" src={electronLogo} />
+        <h1 className="text-5xl">Inventory XI</h1>
       </div>
-      <p className="tip">
-        Please try pressing <code>F12</code> to open the devTool
-      </p>
-      <div className="actions">
-        <div className="action">
-          <a href="https://electron-vite.org/" target="_blank" rel="noreferrer">
-            Documentation
-          </a>
-        </div>
-        <div className="action">
-          <a target="_blank" rel="noreferrer" onClick={ipcHandle}>
-            Send IPC
-          </a>
-        </div>
-      </div>
-      <Versions></Versions>
     </NextUIProvider>
   )
 }
