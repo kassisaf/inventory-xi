59801920993a43cc30175cfb9d7d5090d2e40373
Andrew Kassis <kassisaf@gmail.com>
2024-05-27 20:01:51 -0700

    Install tailwind

---
+++ tailwind.config.js
@@ -0,0 +1,8 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ['./src/**/*.{js,ts,jsx,tsx}', './out/**/*.{js,ts,jsx,tsx}'],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+}
