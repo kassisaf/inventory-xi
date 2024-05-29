6ee7c3c62004430a58b2545ae343b3058ff1e4b0
Andrew Kassis <kassisaf@gmail.com>
2024-05-28 23:40:06 -0700

    Add refresh icon, tweak footer

--- src/renderer/src/App.tsx
+++ src/renderer/src/App.tsx
@@ -2,19 +2,23 @@ import { Tabs, Tab, Card, CardBody, Button } from '@nextui-org/react'
 import { HiCog8Tooth as SettingsIcon } from 'react-icons/hi2'
 import { SiKofi as CoffeeIcon } from 'react-icons/si'
 import { FaRegQuestionCircle as HelpIcon } from 'react-icons/fa'
+import { MdOutlineRefresh as RefreshIcon } from 'react-icons/md'
 
 function App(): JSX.Element {
   return (
     <div className="flex h-screen grow flex-col justify-between bg-zinc-900 text-zinc-50 dark">
       <header className="flex items-center justify-between bg-zinc-700/50 px-6 py-5">
         <h1 className="bold text-3xl">Inventory XI</h1>
-        <div className="flex items-center gap-2">
+        <div className="flex items-center gap-3">
           {/* <CoffeeIcon size="32px" className="text-rose-300" /> */}
 
-          <Button isIconOnly aria-label="Like">
+          <Button isIconOnly aria-label="Refresh">
+            <RefreshIcon size="32px" />
+          </Button>
+          <Button isIconOnly aria-label="Settings">
             <SettingsIcon size="32px" />
           </Button>
-          <Button isIconOnly aria-label="Take a photo">
+          <Button isIconOnly aria-label="About">
             <HelpIcon size="28px" />
           </Button>
         </div>
@@ -42,9 +46,12 @@ function App(): JSX.Element {
         </div>
       </main>
 
-      <footer className="flex gap-x-6 bg-zinc-700/50 px-6 py-2">
-        <div>#/# slots used</div>
-        <div># recoverable</div>
+      <footer className="flex justify-between bg-zinc-700/50 px-6 py-2 text-zinc-200">
+        <div className="flex gap-x-6">
+          <div>#/# slots used</div>
+          <div># recoverable</div>
+        </div>
+        <div className="text-md italic">Parsed 22,356 items in 2.6 seconds</div>
       </footer>
     </div>
   )
