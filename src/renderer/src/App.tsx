c010c824d758d2d7a5cde95a32d1abae7149d49d
Andrew Kassis <kassisaf@gmail.com>
2024-05-28 23:29:38 -0700

    Rework header

--- src/renderer/src/App.tsx
+++ src/renderer/src/App.tsx
@@ -1,15 +1,27 @@
-import { Tabs, Tab, Card, CardBody } from '@nextui-org/react'
-import { HiMiniUser as UserIcon } from 'react-icons/hi2'
+import { Tabs, Tab, Card, CardBody, Button } from '@nextui-org/react'
+import { HiCog8Tooth as SettingsIcon } from 'react-icons/hi2'
+import { SiKofi as CoffeeIcon } from 'react-icons/si'
+import { FaRegQuestionCircle as HelpIcon } from 'react-icons/fa'
 
 function App(): JSX.Element {
   return (
-    <div className="flex h-screen grow flex-col justify-between bg-zinc-900 text-zinc-50">
-      <header className="flex justify-center bg-zinc-700/50 px-6 pb-3 pt-1">
+    <div className="flex h-screen grow flex-col justify-between bg-zinc-900 text-zinc-50 dark">
+      <header className="flex items-center justify-between bg-zinc-700/50 px-6 py-5">
         <h1 className="bold text-3xl">Inventory XI</h1>
+        <div className="flex items-center gap-2">
+          {/* <CoffeeIcon size="32px" className="text-rose-300" /> */}
+
+          <Button isIconOnly aria-label="Like">
+            <SettingsIcon size="32px" />
+          </Button>
+          <Button isIconOnly aria-label="Take a photo">
+            <HelpIcon size="28px" />
+          </Button>
+        </div>
       </header>
 
       <main className="grow px-3 py-2">
-        <div className="flex w-full flex-col dark">
+        <div className="flex w-full flex-col">
           <Tabs aria-label="Options" variant="underlined">
             <Tab key="photos" title="Zurirose">
               <Card>
@@ -30,7 +42,7 @@ function App(): JSX.Element {
         </div>
       </main>
 
-      <footer className="flex gap-x-6 bg-zinc-700/50 px-6 py-1">
+      <footer className="flex gap-x-6 bg-zinc-700/50 px-6 py-2">
         <div>#/# slots used</div>
         <div># recoverable</div>
       </footer>
